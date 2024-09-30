e31d2dd6014b8e69020fa7617e63b947dc34849c
Arpit Jana <arpitjana2103@gmail.com>
2024-09-30 18:33:52 +0530

    getAll Tours, and get tour by id controller added

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -18,5 +18,35 @@ exports.createTour = async function (req, res) {
 };
 
 exports.getAllTours = async function (req, res) {
-    const allTours = await Tour.find();
+    try {
+        const allTours = await Tour.find();
+        return res.status(200).json({
+            status: "success",
+            data: {
+                tours: allTours,
+            },
+        });
+    } catch (error) {
+        return res.status(400).json({
+            status: "fail",
+            error: error,
+        });
+    }
+};
+
+exports.getTour = async function (req, res) {
+    try {
+        const { id } = req.params;
+        const tour = await Tour.findById(id);
+
+        return res.status(200).json({
+            status: "success",
+            tour: tour,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            status: "fail",
+            error: error,
+        });
+    }
 };
