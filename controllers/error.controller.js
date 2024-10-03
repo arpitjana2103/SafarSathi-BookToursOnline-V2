3b38f5ae20919f06f3bb02cfa460569cd4f7040a
Arpit Jana <arpitjana2103@gmail.com>
2024-10-03 18:25:32 +0530

    Handled Cast Error & Duplicate Field Error & Validation Error

--- controllers/error.controller.js
+++ controllers/error.controller.js
@@ -1,3 +1,5 @@
+const AppError = require("../utils/AppError");
+
 const sendErrForDev = function (err, res) {
     return res.status(err.statusCode).json({
         status: err.status,
@@ -7,7 +9,40 @@ const sendErrForDev = function (err, res) {
     });
 };
 
+const handleCastErrorDB = function (err) {
+    if (err.name === "CastError") {
+        const message = `Invalid >> ${err.path} = "${err.value}"`;
+        return new AppError(message, 400);
+    }
+    return err;
+};
+
+const handleDuplicateFieldsDB = function (err) {
+    if (err.code === 11000) {
+        const message = `Duplicate field value >> ${JSON.stringify(err.keyValue)}`;
+        return new AppError(message, 400);
+    }
+    return err;
+};
+
+const handleValidationError = function (err) {
+    if (err.name === "ValidationError") {
+        const message = err.message;
+        return new AppError(message, 400);
+    }
+    return err;
+};
+
 const sendErrForProd = function (err, res) {
+    // Handle DB Cast Error
+    err = handleCastErrorDB(err);
+
+    // Handle Duplicate Field Value Error
+    err = handleDuplicateFieldsDB(err);
+
+    // Handle Validation Error
+    err = handleValidationError(err);
+
     if (err.isOperational) {
         return res.status(err.statusCode).json({
             status: err.status,
@@ -15,7 +50,7 @@ const sendErrForProd = function (err, res) {
         });
     }
     // Unknown Error Handelling in Production
-    console.error(err);
+    // console.error(err);
     return res.status(500).json({
         status: "error",
         message: "Something went very wrong !",
