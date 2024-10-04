7357879a12bbc06e8162a955749d754a0a6a3fd1
Arpit Jana <arpitjana2103@gmail.com>
2024-10-04 13:25:27 +0530

    signup user error fix

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,12 @@ const User = require("../models/user.model");
 const { catchAsyncErrors } = require("./error.controller");
 
 exports.signup = catchAsyncErrors(async function (req, res, next) {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+    });
 
     return res.status(200).json({
         status: "success",
