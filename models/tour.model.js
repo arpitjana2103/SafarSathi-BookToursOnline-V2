32f5661b539041a5252cd9a6618b12149c1c7c63
Arpit Jana <arpitjana2103@gmail.com>
2024-12-03 11:49:17 +0530

    Embedding user in Tours

--- models/tour.model.js
+++ models/tour.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const User = require("./user.model");
 
 const tourSchema = new mongoose.Schema(
     {
@@ -118,6 +119,7 @@ const tourSchema = new mongoose.Schema(
                 day: Number,
             },
         ],
+        guides: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
     },
     {
         toJSON: { virtuals: true },
@@ -140,6 +142,15 @@ tourSchema.pre("save", function (next) {
     this.slug = slugify(this.name, { lower: true });
     next();
 });
+
+// Embeding Tour-Guides ( Not Applicable for this application )
+// tourSchema.pre("save", async function (next) {
+//     const guidesPromises = this.guides.map(
+//         async (id) => await User.findById(id),
+//     );
+//     this.guides = await Promise.all(guidesPromises);
+//     next();
+// });
 
 // runs after Model.prototype.save() and Model.create()
 tourSchema.post("save", function (doc, next) {
